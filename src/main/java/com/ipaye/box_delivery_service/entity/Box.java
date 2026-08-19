37ab1452e0f7ea0e2ea615e115c3c41427ec85d9
mac <@b.tunde.ipaye@gmail.com>
2026-08-19 05:30:01 +0100

    "refactor: improve box entity persistence mapping"

--- src/main/java/com/ipaye/box_delivery_service/entity/Box.java
+++ src/main/java/com/ipaye/box_delivery_service/entity/Box.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table( name = "boxes" )
---
+++ src/main/java/com/ipaye/box_delivery_service/entity/Item.java
@@ -0,0 +1,4 @@
+package com.ipaye.box_delivery_service.entity;
+
+public class Item {
+}
