28b6279b1a2564c92d01baf395b8024e4955eeaf
mac <@b.tunde.ipaye@gmail.com>
2026-08-19 11:27:21 +0100

    feat: add JPA repository for box

---
+++ src/main/java/com/ipaye/box_delivery_service/repository/BoxRepository.java
@@ -0,0 +1,20 @@
+package com.ipaye.box_delivery_service.repository;
+
+import com.ipaye.box_delivery_service.entity.Box;
+import com.ipaye.box_delivery_service.enums.BoxState;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BoxRepository extends JpaRepository<Box, Long> {
+
+    Optional<Box> findByTxref(String txref);
+
+    Boolean existById(String txref);
+
+    List<Box> findByStateAndBatterCapacityGreaterThanEqual(
+            BoxState state,
+            Integer batterCapacity
+    );
+}
