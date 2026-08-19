d4ed0f403868d1e66ac9d0de20145d3f82f09040
mac <@b.tunde.ipaye@gmail.com>
2026-08-19 10:23:38 +0100

    feat: enhance box entity with persistence mappings

--- src/main/java/com/ipaye/box_delivery_service/entity/Item.java
+++ src/main/java/com/ipaye/box_delivery_service/entity/Item.java
@@ -1,4 +1,33 @@
 package com.ipaye.box_delivery_service.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer weight;
+
+    @Column(nullable = false, length = 100)
+    private String code;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "box_id", nullable = false)
+    private Box box;
 }
