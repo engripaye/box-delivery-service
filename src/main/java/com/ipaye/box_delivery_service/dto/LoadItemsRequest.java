ce0d00cd7ea7adbba078bc8463cdf06a4d07a05b
mac <@b.tunde.ipaye@gmail.com>
2026-08-19 10:51:19 +0100

    feat: create Load item request dto

---
+++ src/main/java/com/ipaye/box_delivery_service/dto/LoadItemsRequest.java
@@ -0,0 +1,12 @@
+package com.ipaye.box_delivery_service.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+
+import java.util.List;
+
+public record LoadItemsRequest(
+        @NotEmpty(message = "At least one item is required")
+        List<@Valid ItemRequest> items
+) {
+}
